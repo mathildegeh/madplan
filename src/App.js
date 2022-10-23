97458661e257010c747abec20dd8defaef4fc480
mathildegeh <mathilde.gehring@hotmail.com>
2022-10-23 14:16:43 +0200

    basename

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Recipes } from "./components/allRecipes";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename={"/madplan"}>
         <NavigationBar />
         <Routes>
           <Route path="/" element={<Mealplan />} />

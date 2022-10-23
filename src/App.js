48efaf3c717e931ef828dc72319ec8d2a87a9ac3
mathildegeh <mathilde.gehring@hotmail.com>
2022-10-23 13:50:03 +0200

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
 import { NavigationBar } from "./components/navbar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Mealplan } from "./components/makeMealplan";
 import { Recipes } from "./components/allRecipes";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <NavigationBar />
         <Routes>
           <Route path="/madplan" element={<Mealplan />} />
           <Route path="/madplan/all-recipes" element={<Recipes />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
